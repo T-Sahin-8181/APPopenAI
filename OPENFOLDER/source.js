7fe7bbade009813141a6bc58268fc21182235b86
Talha S. <talha.sahin@docc.techstarter.de>
2023-12-20 00:36:32 +0100

    BETA VERSION without API

---
+++ OPENFOLDER/source.js
@@ -0,0 +1,31 @@
+// HTML elements
+const formElement = document.querySelector("form");
+const outputElement = document.getElementById("output");
+const textArea = document.querySelector("textarea");
+
+// handhabung form Eingaben 
+formElement.addEventListener("submit", async (e) => {
+    // Damit die seite nicht alles neu Lädt
+    e.preventDefault();
+   
+    //lade status
+    outputElement.innerText = "Loading..";
+
+	// make a POST request to /completion endpoint with textarea's text
+	const response = await fetch("/completion", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ message: textArea.value }),
+	});
+
+	// convert the response to json
+	const data = await response.json();
+
+	// show the answer resp. the error
+	if (data.answer) {
+		outputElement.innerText = data.answer;
+	} else if (data.error) {
+		outputElement.innerText = "";
+		window.alert(data.error);
+	}
+});
--- main.js
+++ main.js
@@ -1,10 +1,71 @@
-import  express  from "express";
+import  express, { response }  from "express";
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log("der server läuft, is running", PORT);
-})
+});
 
-app.get("/", (req, res) => res.send("Der server läuft!"))
+// Server zur statischen dateien, und schickt json dateien
+app.use(express.static("OPENFOLDER"));
+app.use(express.json());
+
+// environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+
+
+// setup Open AI
+
+//import { Configuration, OpenAIApi } from "openai";
+//const configuration = new Configuration({
+	apiKey: process.env.API_KEY,
+//});
+//const openai = new OpenAIApi(configuration);
+
+
+
+
+
+
+
+
+
+
+// handle POST request
+app.post("/completion", async (req, res) => {
+	if (!req.body.message) {
+		return res
+			.status(400)
+			.send({ error: "Error: Empty message" });
+	}
+
+	console.log("prompt:", req.body.message, "\n");
+
+	try {
+		const response = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: req.body.message,
+			max_tokens: 2048,
+		});
+
+		const answer = response.data?.choices[0]?.text;
+
+		console.log("answer:", answer, "\n");
+
+		if (response.status == 200 && answer) {
+			return res.status(200).send({ answer });
+		} else {
+			return res.status(500).send({
+				error: "Error: Could not process your query",
+			});
+		}
+	} catch (err) {
+		console.log(err.message);
+		return res
+			.status(500)
+			.send({ error: "Error: Could not process your query" });
+	}
+});
